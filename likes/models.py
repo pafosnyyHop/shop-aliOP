40998693cb85037655b09219eb73f73014b1c595
tokami3d <dzholdoshev1@gmail.com>
2023-01-24 20:38:52 +0600

    remoted

--- likes/models.py
+++ likes/models.py
@@ -6,7 +6,7 @@ class Like(models.Model):
     owner = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE,
                               related_name='liked_posts')
     product = models.ForeignKey(Product, on_delete=models.CASCADE,
-                             related_name='likes')
+                                related_name='likes')
 
     class Meta:
         unique_together = ['owner', 'product']
