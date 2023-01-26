a18bce3d58102b50f6969072eeece693375ce046
pafosnyyHop <odecik30@gmail.com>
2023-01-26 14:43:52 +0600

    added rating

--- rating/serializers.py
+++ rating/serializers.py
@@ -4,10 +4,16 @@ from .models import Rating
 
 class RatingSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.id')
-    user_email = serializers.ReadOnlyField(source='user.email')
-    product = serializers.ReadOnlyField(source='product.id')
 
     class Meta:
         model = Rating
         fields = '__all__'
 
+    def create(self, validated_data):
+        # print(self, '!!!!!!!!!!!!!!!!!!!!!')
+        rating = Rating.objects.update_or_create(
+            user=validated_data.get('user', None),
+            product=validated_data.get('product', None),
+            defaults={'rating': validated_data.get("rating")}
+        )
+        return rating
